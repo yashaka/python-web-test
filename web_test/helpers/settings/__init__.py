56412b3c4f179dd214a72e35a224b869607f9365
Iakiv Kramarenko <yashaka@gmail.com>
2021-03-11 15:21:41 +0200

    commented custom configuration management support (let's move to pydantic for now, yet keep these helpers just in case...)

--- web_test/helpers/settings/__init__.py
+++ web_test/helpers/settings/__init__.py
@@ -1,1 +1,7 @@
-from . import sourced
+from . import sourced, source
+"""
+An alternative implementation of Project Configuration management.
+Not used in this project yet. 
+- But you can consider it for more control and 
+  less dependencies from external libs like Pydantic, etc.
+"""
